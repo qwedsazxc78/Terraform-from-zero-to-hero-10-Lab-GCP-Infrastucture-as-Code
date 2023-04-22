7bd5e4f0eba31705ce4f2c1eb88a237ed69fd541
alexhsieh <alexhsieh@awoo.com.tw>
2023-04-22 15:54:15 +0800

    update path

--- Chap3-Terrafrom-基礎篇/Ch3-1-7-Provisioners-配置器的概念與使用/main.tf
+++ Chap3-Terrafrom-基礎篇/Ch3-1-7-Provisioners-配置器的概念與使用/main.tf
@@ -22,18 +22,21 @@ resource "google_compute_instance" "example" {
     }
   }
 
+  # 失敗案例，無法連線到遠端
   provisioner "remote-exec" {
     inline = [
-      "echo ${google_compute_instance.example.network_interface[0].network_ip} > ip_address_remote_exec.txt"
+      "echo ${google_compute_instance.example.network_interface[0].network_ip} > /tmp/ip_address_remote_exec.txt"
     ]
   }
 
+  # 成功案例，虛擬電腦本機執行
   provisioner "local-exec" {
-    command = "echo ${google_compute_instance.example.network_interface[0].network_ip} > ip_address_local_exec.txt"
+    command = "echo ${google_compute_instance.example.network_interface[0].network_ip} > /tmp/ip_address_local_exec.txt"
   }
 
+  # 成功案例，傳送到虛擬電腦本機
   provisioner "file" {
     content     = google_compute_instance.example.network_interface[0].network_ip
-    destination = "./ip_address_file.txt"
+    destination = "/tmp/ip_address_file.txt"
   }
 }
