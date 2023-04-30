7191e91bda095c48091c5ad0d8b217589c591cc8
alexhsieh <alexhsieh@awoo.com.tw>
2023-04-30 22:56:50 +0800

    update labs

---
+++ Chap4-GCP雲端與IAC架構實戰/Ch4-4-4-Lab5:創建虛擬機器-VM-使用地區硬碟Regitional-Disk與startup-script-產生-nginx服務器/2-main.tf
@@ -0,0 +1,42 @@
+##################################################################################
+# RESOURCE
+##################################################################################
+resource "google_compute_instance" "example" {
+  name         = var.instance_name
+  zone         = var.zone
+  machine_type = "e2-medium"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+  # 安裝 nginx
+  metadata_startup_script = "sudo apt update -y && sudo apt install nginx -y"
+}
+
+resource "google_compute_region_disk" "example" {
+  name   = "example-disk"
+  type   = "pd-standard"
+  region = var.location
+  size   = 200
+
+  replica_zones = ["asia-northeast1-a", "asia-northeast1-b"]
+}
+
+resource "google_compute_attached_disk" "example" {
+  disk     = google_compute_region_disk.example.self_link
+  instance = google_compute_instance.example.self_link
+  mode     = "READ_WRITE"
+}
+
+output "instance_ip" {
+  value = google_compute_instance.example.network_interface[0].access_config[0].nat_ip
+}
