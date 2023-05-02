9dbcc5ff31623174a594f67674a656257dd3ef33
alexhsieh <alexhsieh@awoo.com.tw>
2023-05-03 01:28:21 +0800

    update lab

--- Chap4-GCP雲端與IAC架構實戰/Ch4.6-4.10/Ch4.10.3-GCP-資料庫服務-使用Terraform創建關聯式-SQL-DB/2-main.tf
+++ Chap4-GCP雲端與IAC架構實戰/Ch4.6-4.10/Ch4.10.3-GCP-資料庫服務-使用Terraform創建關聯式-SQL-DB/2-main.tf
@@ -2,31 +2,25 @@
 # RESOURCE
 ##################################################################################
 resource "google_sql_database_instance" "instance" {
-  name             = "example-instance"
-  database_version = "POSTGRES_14"
+  name                = var.db_name
+  database_version    = "POSTGRES_14"
+  deletion_protection = false
 
   settings {
-    tier = "db-f1-micro" # 使用最簡單的 CPU 和硬碟空間設定
-
-    ip_configuration {
-      ipv4_enabled                                  = true
-      private_network                               = "projects/${var.GCP_PROJECT}/global/networks/default"
-      enable_private_path_for_google_cloud_services = true
-
-
-      dynamic "authorized_networks" {
-        for_each = ["0.0.0.0/0"] # 允許從任意 IP 地址訪問資料庫
-        content {
-          value = authorized_networks.key
-        }
-      }
-    }
+    tier      = "db-n1-standard-2" # 使用標準的硬體配備
+    disk_size = "10"
   }
 }
 
 resource "google_sql_database" "this" {
   name     = var.db_name
   instance = google_sql_database_instance.instance.name
+}
+
+resource "google_sql_user" "users" {
+  name     = "admin"
+  instance = google_sql_database_instance.instance.name
+  password = "admin!@#$%"
 }
 
 output "db_ip" {
