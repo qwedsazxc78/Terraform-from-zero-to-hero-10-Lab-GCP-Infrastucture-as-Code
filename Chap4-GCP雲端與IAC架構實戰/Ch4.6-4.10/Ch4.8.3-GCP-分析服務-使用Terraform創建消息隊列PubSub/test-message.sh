0a758916ef2738b2c34132ab3d893870aed2b6ad
alexhsieh <alexhsieh@awoo.com.tw>
2023-07-02 19:07:45 +0800

    update limit

--- Chap4-GCP雲端與IAC架構實戰/Ch4.6-4.10/Ch4.8.3-GCP-分析服務-使用Terraform創建消息隊列PubSub/test-message.sh
+++ Chap4-GCP雲端與IAC架構實戰/Ch4.6-4.10/Ch4.8.3-GCP-分析服務-使用Terraform創建消息隊列PubSub/test-message.sh
@@ -2,4 +2,4 @@ gcloud pubsub topics publish \
 "projects/terraform101-384507/topics/pubsub-topic-tf-ch-4-8-3" \
     --message "Hello, World"
 
-gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch-4-8-3
+gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch-4-8-3 --limit=10
--- Chap4-GCP雲端與IAC架構實戰/Ch4.6-4.10/Ch4.8.4-Lab9-創建一個消息隊列PubSub-並嘗試發送與訂閱訊息/test_message.sh
+++ Chap4-GCP雲端與IAC架構實戰/Ch4.6-4.10/Ch4.8.4-Lab9-創建一個消息隊列PubSub-並嘗試發送與訂閱訊息/test_message.sh
@@ -7,20 +7,20 @@ gcloud pubsub topics publish "projects/terraform101-384507/topics/pubsub-topic-t
     --message "Hello, World user a" \
     --attribute=user=a
 
-gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch4-lab9-a
+gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch4-lab9-a --limit=10
 
 # 訂閱者b
 gcloud pubsub topics publish "projects/terraform101-384507/topics/pubsub-topic-tf-ch4-lab9" \
     --message "Hello, World user b" \
     --attribute=user=b
 
-gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch4-lab9-b
+gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch4-lab9-b --limit=10
 
 # 訂閱者b (a會被覆蓋)
 gcloud pubsub topics publish "projects/terraform101-384507/topics/pubsub-topic-tf-ch4-lab9" \
     --message "Hello, World user a & b" \
     --attribute=user=a,user=b
 
-gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch4-lab9-a
+gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch4-lab9-a --limit=10
 
-gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch4-lab9-b
+gcloud pubsub subscriptions pull projects/terraform101-384507/subscriptions/pubsub-sub-tf-ch4-lab9-b --limit=10
