9de0536ec68b18db1ef909793acd55d781bec233
alexhsieh <alexhsieh@awoo.com.tw>
2023-07-08 22:54:41 +0800

    example

--- Chap4-GCP雲端與IAC架構實戰/Ch4.11-End/Ch4.12.2-GCP-資料庫服務-使用GCP-Console創建memory-store-redis/ssh-forward.sh
+++ Chap4-GCP雲端與IAC架構實戰/Ch4.11-End/Ch4.12.2-GCP-資料庫服務-使用GCP-Console創建memory-store-redis/ssh-forward.sh
@@ -1,3 +1,3 @@
-# gcloud compute ssh [VM名稱] --project [專案ID] --zone [區域] -- -L [本機端口]:localhost:[Redis端口]
+# gcloud compute ssh [VM名稱] --project [專案ID] --zone [區域] -- -L [本機端口]:[redis ip]]:[Redis端口]
 
-gcloud compute ssh [VM名稱] --project [專案ID] --zone [區域] -- -L 6379:localhost:6379
+gcloud compute ssh --zone "asia-northeast1-b" "redis-tf-ch4-12-2-connection" --project "terraform101-384507" -- -L 6379:10.143.79.147:6379
